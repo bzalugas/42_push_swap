69d92c1d0245e3a3bf7c0c0fac5f2548f901a75d
bazaluga <bazaluga@student.42.fr>
2024-02-29 15:47:21 +0100

    quicksort implementation

---
+++ src/quicksort.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quicksort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 12:24:20 by bazaluga          #+#    #+#             */
+/*   Updated: 2024/02/29 15:45:54 by bazaluga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+#include <stdio.h>
+
+static void swap(int *i, int *j)
+{
+	int	tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+void	display_arr(int *arr, int len)
+{
+	for (int i = 0; i < len; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+static int	partition(int *arr, int l, int r)
+{
+	int	p;
+	int	i;
+	int	j;
+
+	p = r;
+	i = l - 1;
+	j = l;
+	while (j < r)
+	{
+		if (arr[j] < arr[p] && ++i != j)
+			swap(&arr[i], &arr[j]);
+		j++;
+	}
+	i++;
+	swap(&arr[i], &arr[r]);
+	return (i);
+}
+
+void	quicksort(int *arr, int l, int r)
+{
+	int	p;
+
+	if (l >= r)
+		return ;
+	p = partition(arr, l, r);
+	quicksort(arr, l, p - 1);
+	quicksort(arr, p, r);
+}
+int main()
+{
+	int	arr[] = {5, -12, 8, 23, -58, 26, 2};
+	int	len = sizeof(arr) / sizeof(int);
+	printf("Before : ");
+	display_arr(arr, len);
+	quicksort(&arr[0], 0, len - 1);
+	printf("After : ");
+	display_arr(arr, len);
+	return (0);
+}
