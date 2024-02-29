8003021b332b6901c6b5511f3b52ad01d2a415fc
bazaluga <bazaluga@student.42.fr>
2024-02-29 16:16:04 +0100

    feat: more simple testing quicksort

--- src/quicksort.c
+++ src/quicksort.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 12:24:20 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/02/29 15:45:54 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:12:25 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,18 @@ static int	partition(int *arr, int l, int r)
 	int	i;
 	int	j;
 
+	/* printf("partition\n"); */
 	p = r;
 	i = l - 1;
 	j = l;
 	while (j < r)
 	{
 		if (arr[j] < arr[p] && ++i != j)
+		{
+			printf("swap\n");
 			swap(&arr[i], &arr[j]);
+		}
+
 		j++;
 	}
 	i++;
@@ -53,20 +58,34 @@ void	quicksort(int *arr, int l, int r)
 {
 	int	p;
 
+	/* printf("\033[31mquicksort\n\033[0m"); */
 	if (l >= r)
 		return ;
 	p = partition(arr, l, r);
 	quicksort(arr, l, p - 1);
 	quicksort(arr, p, r);
 }
-int main()
+#include <ctype.h>
+int main(int ac, char *av[])
 {
-	int	arr[] = {5, -12, 8, 23, -58, 26, 2};
+	(void)ac;
+	int i = 0;
+	int j = 0;
+	int arr[500];
+	while (av[1][i])
+	{
+		arr[j] = atoi(&av[1][i]);
+		while (av[1][i] && isdigit(av[1][i]))
+			i++;
+		i++;
+		j++;
+	}
+	/* int	arr[] = {5, -12, 8, 23, -58, 26, 2}; */
 	int	len = sizeof(arr) / sizeof(int);
-	printf("Before : ");
-	display_arr(arr, len);
+	/* printf("Before : "); */
+	/* display_arr(arr, len); */
 	quicksort(&arr[0], 0, len - 1);
-	printf("After : ");
-	display_arr(arr, len);
+	/* printf("After : "); */
+	/* display_arr(arr, len); */
 	return (0);
 }
