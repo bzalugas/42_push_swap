0a41925ac8404b745970d0325ca697e9c0ff1fcb
bazaluga <bazaluga@student.42.fr>
2024-03-05 15:18:16 +0100

    Continue translate (but will change strategy)

--- src/translate_swap.c
+++ src/translate_swap.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 09:53:52 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/03/05 11:34:43 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:18:09 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ void int_swap(int *i, int *j)
 	*j = tmp;
 }
 
-static void	swap_adj_els(t_stacks *s, t_frame *f1)
+static void	swap_adj_els(t_stacks *s, t_frame *f)
 {
 	int		count;
 	char	op;
 
 	count = 0;
 	op = 1;
-	if (f1->i >= s->a->size / 2)
+	if (f->i >= s->a->size / 2)
 		op = 2;
-	while (f1->i > 0) //&& ++count
+	while (f->i > 0) //&& ++count
 	{
 		if (op == 1)
 			ra(s);
@@ -49,13 +49,21 @@ static void	swap_adj_els(t_stacks *s, t_frame *f1)
 	}
 }
 
-static void	pushb_el(t_stacks *s, t_frame *f1)
+static void	pushb_el(t_stacks *s, t_frame *f)
 {
 	char	op;
 
 	op = 1;
-	if (f1->i >= s->a->size / 2)
+	if (f->i >= s->a->size / 2)
 		op = 2;
+	while (f->i > 0)
+	{
+		if (op == 1)
+			ra(s);
+		else
+			rra(s);
+
+	}
 }
 
 static void	swap_els(t_stacks *s, t_frame *f1, t_frame *f2)
