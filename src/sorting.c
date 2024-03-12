9850a6837570a9e17699e6b30e9469aaa2bc3bf0
bazaluga <bazaluga@student.42.fr>
2024-03-12 13:58:12 +0100

    feat: begin again sorting

--- src/sorting.c
+++ src/sorting.c
@@ -6,87 +6,38 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:50:45 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/03/11 16:03:25 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/03/12 13:58:01 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-t_op	*get_stack_cmds(t_stacks *s, t_stack *stack)
+void sort_three(t_stacks *s)
 {
-	static t_op	cmd_a[3] = {SA, RA, RRA};
-	static t_op	cmd_b[3] = {SB, RB, RRB};
+	t_stack	*a;
 
-	if (stack == s->a)
-		return (cmd_a);
-	return (cmd_b);
-}
-
-t_list *sort_three(t_stacks *ss, t_stack *s)
-{
-	t_list	*tmp;
-	t_op	*cmds;
-
-	cmds = get_stack_cmds(ss, s);
-	tmp = ft_lstlast(ss->cmd);
-	if (s->size < 3)
-		return (NULL);
-	if (s->top == stack_get_max(s) && ft_lstadd_back2(&ss->cmd, (void *)cmds[2]))
-		rotate(s);
-	if (s->top->next->n > s->bot->n && ft_lstadd_back2(&ss->cmd, (void *)RRO))
-		rrotate(s);
-	if (s->top->n > s->top->next->n && ft_lstadd_back2(&ss->cmd, (void *)S))
-		swap(s);
-	return (tmp->next);
-}
-
-void	sort_five(t_stack *s)
-{
-	t_frame	*f;
-
-	if (s->size < 4 || s->size > 5)
+	a = s->a;
+	if (a->size < 3)
 		return ;
-	while (stack_score(s) < s->size)
-	{
-		f = s->top;
-		while (f->i < (s->size + (s->size % 2 != 0)) / 2)
-		{
-			if (f->n > f->next->n) //+ use median ?
-			{
-				if (f == stack_get_max(s))
-					rotate(s);
-			}
-			f = f->next;
-		}
-	}
+	if (a->top == stack_get_max(a))
+		sa(s);
+	if (a->top->next->n > a->bot->n)
+		rra(s);
+	if (a->top->n > a->top->next->n)
+		sa(s);
 }
 
-void	sort_b(t_stacks *s)
+void	push_non_sorted(t_stacks *s)
 {
-	if (s->b->size == 3)
-		sort_three(s->b);
-	else
-	{
 
-	}
 }
 
 void	stacks_sort(t_stacks *s)
 {
-	t_frame	*a;
-	t_frame	*b;
-
 	if (stack_score(s->a) == s->a->size)
 		return ;
-	while (stack_score(s->a) < s->a->size + s->b->size)
+	while (stack_score(s->a) < s->total)
 	{
-		a = s->a->top;
-		b = s->b->top;
-		if (a && a->next && a->n > a->next->n)
-		{
-			if (s->b->size >= 3 && stack_score(s->b) < s->b->size)
-				sort_b(s);
-			pb(s);
-		}
+
 	}
 }
