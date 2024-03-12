4e4c44dcf8b9cfb920835ac58ddaae8e4f591cd2
bazaluga <bazaluga@student.42.fr>
2024-03-12 15:01:28 +0100

    feat: continue sorting

--- src/sorting.c
+++ src/sorting.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:50:45 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/03/12 13:58:01 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/03/12 15:01:09 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,26 @@ void sort_three(t_stacks *s)
 		sa(s);
 }
 
+int	cost_push_b(t_stacks *s, t_frame *f, t_frame *t)
+{
+	int	cost;
+	int	med_a;
+	int	med_b;
+
+	med_a = s->a->size + (s->a->size % 2 != 0) / 2;
+	med_b = s->b->size + (s->b->size % 2 != 0) / 2;
+	if ((f->i <= med_a && t->i <= med_b) || (f->i > med_a && t->i > med_b))
+		cost = ft_max_int(f->i, t->i) - ft_min_int(f->i, t->i);
+	else
+		cost = f->i + t->i;
+	return (cost + 1);
+}
+
 void	push_non_sorted(t_stacks *s)
 {
+	t_frame	*cheapest;
+	int		min_cost;
+
 
 }
 
@@ -36,8 +54,12 @@ void	stacks_sort(t_stacks *s)
 {
 	if (stack_score(s->a) == s->a->size)
 		return ;
-	while (stack_score(s->a) < s->total)
+	while (s->a->size > 3 && stack_score(s->a) < s->total)
 	{
 
 	}
 }
+
+/*
+** find 2 non sorted nbs. push the one that costs less to b.
+*/
